c9e6332a7b12256f6b57f09740131ae61ee13d13
m-teja <36612521+m-teja@users.noreply.github.com>
2018-03-10 12:48:10 -0800

    Version 0.2
    Displayed single keycode

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+
+//Detects key presses
 document.body.addEventListener('keypress', function(ev) {
     console.log(ev.keyCode);
+    fullList.push(ev.keyCode);
+    chrome.storage.local.set({ "keyPressed" : ev.keyCode }, function () {
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
+        
+    });
 });
+
+//loads onto html page
+function load() {
+    chrome.storage.local.get("keyPressed", function (items) {
+        if (!chrome.runtime.error) {
+            console.log("recieved" + items.keyPressed);
+            document.getElementById("display").innerHTML = items.keyPressed;
+        }
+    });
+}
+document.getElementById("update").onclick = load;

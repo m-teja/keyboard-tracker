673c17a2260476c0ce997ba4ade0d3e2d099b7f4
m-teja <36612521+m-teja@users.noreply.github.com>
2018-03-10 21:53:15 -0800

    Version 0.3
    added list of keys to allStats

--- main.js
+++ main.js
@@ -1,23 +1,70 @@
-
+var tempList = [];
+var fullArray = [];
 //Detects key presses
-document.body.addEventListener('keypress', function(ev) {
+document.body.addEventListener('keypress', function (ev) {
     console.log(ev.keyCode);
-    fullList.push(ev.keyCode);
-    chrome.storage.local.set({ "keyPressed" : ev.keyCode }, function () {
+    tempList.push(ev.keyCode);
+    chrome.storage.local.set({ "keyPressed" : tempList }, function () {
         if (chrome.runtime.error) {
             console.log("Runtime error.");
         }
+        console.log(tempList);
+        testing();
         
     });
 });
 
+
+
 //loads onto html page
 function load() {
-    chrome.storage.local.get("keyPressed", function (items) {
-        if (!chrome.runtime.error) {
-            console.log("recieved" + items.keyPressed);
-            document.getElementById("display").innerHTML = items.keyPressed;
-        }
+    chrome.storage.local.get("fullList", function (getList) { 
+        chrome.storage.local.get("keyPressed", function (items) {
+            if (!chrome.runtime.error) {
+                console.log("recieved" + items.keyPressed);
+                document.getElementById("display").innerHTML = items.keyPressed;
+                var tempAry = getList.fullList;
+                console.log(tempAry);
+                for (i = 0; i < items.keyPressed.length; i++) {
+                    var tempPress = items.keyPressed[i];
+                    tempAry[tempPress - 1] = tempPress;
+                }
+                console.log(tempAry);
+                save(tempAry);
+            }
+        });
     });
 }
 document.getElementById("update").onclick = load;
+
+function save(tempAry) {
+    chrome.storage.local.set({ "fullList" : tempAry}, function () {
+        console.log(tempAry);
+    });
+}
+
+function reset() {
+    fullArray[199] = 0;
+    fullArray.fill(0, 0, 200);
+    chrome.storage.local.set({ "fullList" : fullArray }, function () {
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
+        document.getElementById("list").innerHTML = fullArray;
+        console.log(fullArray);
+        
+    });
+}
+document.getElementById("reset").onclick = reset;
+
+
+
+/*
+function testing() {
+    chrome.storage.local.get("keyPressed", function (items) {
+        if (!chrome.runtime.error) {
+            console.log("recieved" + items.keyPressed);
+        }
+    });
+}
+*/
